311f5b33d325503fc365605d0117ec7784b685c1
arunnreddy <chinthaarun.1997@gmail.com>
2023-03-31 20:59:02 +0530

    data

---
+++ App.js
@@ -0,0 +1,653 @@
+// let promise=new Promise(function(x,y){
+//   let   isclean=false
+//   if(isclean){
+//      x("hai")
+//   }else{
+//      y("jai")
+//   }
+// })
+// promise.then(function(v){
+//     console.log("andariki",v)
+// }).catch(function(t){
+//     console.log("andariki",t)
+// })
+
+
+// function func1(a,b,cbf){
+//    return  cbf(a,b)
+// }
+// func1(1,2,arun)
+// function arun(c,d){
+// console.log(c+d)
+// }
+
+// const arun=fetch("https://jsonplaceholder.typicode.com/users").then(res=>res.json()).then(varun=>console.log(varun[5]))
+// // console.log("arun")
+// const arun=(a,b)=>{
+//     console.log (a+b)
+// }
+// arun(1,2)
+
+// const vau=function(a,b){
+//     return(a+b)
+
+// }
+// console.log(vau(1,5))
+
+// function wag(){
+//  return  new Promise(function(resolve,reject){
+//     let a=10
+//     if(a>20){
+//         resolve("hai")
+//     }else{
+//         reject("hello")
+//     }
+// })
+// }
+// wag().then((v)=>{
+//     console.log(v)
+// }).catch((n)=>{
+//     console.log(n)
+// })
+
+
+
+// function func1(cbf){
+//     var a=prompt("please enter your name")
+//      return cbf(a)
+// }
+// func1(func2)
+
+// function func2(c){
+// document.write(`hai  ${c}  welcome to the coding world`)
+
+// }
+
+// 
+// var c=20
+// function arun(){
+//     var a=10;
+//     var b=20;
+//     return function varun(){
+//         console.log(a+b+c)
+//     }
+// }
+// let g=arun()
+// g()
+// let arun={
+//     class:"10th",
+//     age:16,
+//     pincode:373883
+          
+//     }
+
+// let varunn={
+   
+
+// }
+// console.log(arun)
+
+// const returnedTarget = Object.assign(varunn, arun);
+
+
+
+// console.log(varunn)
+// delete vinod.class
+// console.log(arun)
+
+
+
+// console.log(returnedTarget)
+
+
+// function arun(cbf){
+//    cbf()
+//    console.log("hello")
+// }
+
+// arun(hai)
+// function hai(){
+//     console.log("iam form hI")
+// }
+
+
+// function arun(a,b,cbf){
+// cbf(a,b)
+// return a+b
+// }
+// console.log(arun(1,2,function(c,d){
+//     console.log(c+d)
+// }))
+
+
+// let arun={
+//     Class:"10th",
+//     age:20
+// }
+
+// let {Class, age}=arun
+// console.log(Class)
+
+
+//  let vali =new Promise (function(resolve,reject){
+// let a=10
+// if(a<16){
+//     reject("nijam")
+// }else{
+//     resolve("abaddamm")
+// }
+// })
+
+// vali.then(function(hai){
+//     console.log(hai+"jai")
+// })
+// vali.catch(function(hello){
+// console.log(hello+"beek")
+// })
+
+
+// async function arun(a,b){
+//    let response= await  a+b
+//    console.log(response)
+
+// }
+
+// console.log(arun(10,20))
+
+
+
+// console.log(a);
+// var a=10;
+// console.log(b);
+// let b=10;
+// console.log(c);
+// const c=10
+
+
+// const a=10;
+// function arun(){
+// console.log(a)
+// const a=10
+// }
+// arun()
+// setTimeout(()=>console.log("arun"),5000)
+
+// setTimeout(function(){
+//     console.log("arun")
+// },2000)
+
+
+
+// let string="arun"
+
+// let reverseString=string.split('').reverse().join('')
+// console.log(reverseString)
+
+
+// let string="hello.jao"
+// let reversingstring=string.split('.')
+// console.log(reversingstring)
+
+
+
+
+// let arun="waste"
+
+// console.log(arun)
+
+// switch(arun){
+//     case "hai every one" :
+//         case "hello every one":
+//             console.log("hello")
+//             break;
+//             case "welcome to the world of coding":
+//                 case "welcome":
+//                     console.log("thank youu")
+//                     break;
+//                     default :
+//                     console.log("wastee")
+// }
+
+
+// let a=10
+// for(i=0;i<=20;i++){
+//    console.log(i)
+//  }
+// let a=0
+// while(a<10){
+//     console.log("hai")
+//     a+=1
+// }
+
+
+
+// async function arun=(a,b)=>{
+//     try{
+//         const hai= await a+b
+//         if(hai<10){
+//         console.log(hai)}
+//     }catch=(err)=>{
+//         console.log("err",err)
+//     }
+// }
+// arun(3,14)
+
+
+//  function hello(a,b){
+// try{
+//     if(a+b>5){
+// console.log(a+b)}
+// }
+// catch(err){
+//     console.log("err",err)
+// }
+//  }
+//  hello(2,22)
+
+// const person={
+//     name:"arun kumar reddy",
+//     surame:"chintha",
+//     age:24
+// }
+// for(p in person){
+//     console.log(p, ":" ,person[p])
+// } 
+
+// person.color="red"
+// let propertyName="color"
+// person[propertyName]="red"
+// console.log(person)
+
+// let dynamicName="name"
+// console.log(person[dynamicName])
+
+
+// for(i=0;i<20;i++){
+//     console.log("hai",i)
+// }
+// how
+
+
+// let  name="arun.jpg"
+// let reversename=name.split("")[4]
+// console.log(reversename)
+
+
+
+ 
+// async function arun(){
+//     try{
+        
+//          const jai=await fetch("https://jsonplaceholder.typicode.com/users").then(res=>res.json()).then(json=>console.log(json)).cach((e)=>console.log("hai",e))
+//      console.log("jai",jai)
+//         }catch(error){
+//             console.log("err",error)
+//         }
+    
+//     }
+// arun()
+
+// let arun={name:"chintha",age:24}
+// console.log(arun.age)
+
+
+// const numbers = [45, 4, 9, 16, 25];
+// let txt = "";
+// numbers.forEach(myFunction);
+
+// function myFunction(value, index, array) {
+//     txt+=value
+// console.log(array)
+// }
+
+
+// let arun={
+//     name:"huwai",
+//     age:23,
+//     salary:23444
+// }
+
+// let naveen=JSON.stringify(arun);
+// console.log(naveen)
+// console.log(arun)
+
+
+
+
+
+// const data=[
+//     {
+//         class:"10th",
+//         name:"arun",
+//         age:23
+
+//     },
+//     {
+//     class:"11th",
+//     name:"vijay",
+//     age:34
+//     }
+// ]
+// console.log(datax
+// const greet=["hello","hai","welocme"]
+// console.log(greet.length)
+// for(i=0;i<=greet.length;i++){
+//     console.log(greet[i])
+// }
+
+// let filename ="arun.csv"
+// console.log(filename)
+// var extension = filename.substr(filename.lastIndexOf("."));
+// console.log(extension)
+// var hello=filename.substr(filename.indexOf("r"))
+// console.log(hello)
+// let array=["1","4","5","6","7","8"]
+// console.log(array.indexOf("8"))
+
+
+
+/* javascript function to validate file type */
+
+
+
+// const files=[{
+//     name:"arun",
+//     age:23
+
+
+// },{
+// name:"varun",
+// age:34
+
+// }]
+
+
+// const hai=files.map((data)=>{return {data}})
+// console.log(hai)
+
+// const arun=[1,2,3,4,5,6]
+
+// const hello=arun.map((data)=>{return {data}})
+// console.log(hello)
+
+
+
+// const numbers=[-2,4,5,6,-8]
+// const positive=numbers.filter((value)=>{
+//     return value>0
+// })
+// console.log(positive)
+
+
+// function myFunction(p1, p2) {
+   
+// document.getElementById("demo").innerHTML = "hai"
+// }
+
+
+
+// let arun="valimai"
+
+// switch(arun){
+//     case arun="hai":
+//     console.log("hello")
+//     break;
+//     case arun="hello":
+//         console.log("jai")
+//         break;
+//         default:
+//             console.log("bai")
+// }
+
+
+// function outer(){
+//     var a=10;
+//     function inner(){
+//         var b=20
+//         return a+b
+//     }
+//     return inner
+// }
+// let arun=outer()
+// console.log(arun())
+
+
+// let arun=""
+// let i=10
+// while(i<20){
+//     arun += "The number is " + i + 
+//     i++
+//     console.log(arun)   
+// }
+// const arun=[{
+//     name:"arun",
+//     age:23,
+//     salary:34566
+// },
+// {
+//     name:"varun",
+//     age:23,
+//     salary:34
+// }]
+// console.log(table.name)
+// const arun=[{airflow:true},{airflow:true}]
+// console.log(arun.map((x)=>x.airflow))
+
+// let arun={
+//     "hai":"job_id:989899"
+// }
+// let c=arun.hai.split(':')
+// let d=c[1].trim()
+// console.log(d)
+
+
+// const arun=
+//     [{
+// name:"varun"
+//     },{
+//       name:"sharath"  
+//     }]
+
+// var Dravid=[]
+
+// for(i=0;i<arun.length;i++){
+//     var naveen=arun[i]
+//     Dravid.push(naveen)
+//     raghu.push(naveen)
+// }
+
+// console.log("hello",Dravid)
+// let arun
+// for (i=0;i<2;i++){
+//  arun=
+//     [{
+// name:"varun"
+//     },{
+//       name:"sharath"  
+//     }]
+
+// }
+
+// console.log(arun)
+// var arun="datannddnt.tsrrrx"
+// const naveen=arun.substr(arun.lastIndexOf('.'))
+// console.log(naveen)
+
+// const arun=[1,2,3,4,5.3,5,5]
+
+var arr=["C","abc","Python","C++","Java"];
+var result= arr.lastIndexOf("C++",4);
+console.log(result)
+
+
+// let arun=[12,-2,0,-3,76,7]
+
+// let naveen=arun.filter((value,index)=>{return value>0})
+// console.log(naveen)
+// let naveen=arun.filter((value,index)=>{return index!==2})
+// console.log(naveen)
+// const hai=delete(arun[0])
+
+
+// let arun=[{
+//     age:13,
+//     class:"10th"
+
+// },{
+// name:"namratha",
+// age:13,
+// class:"10th"
+// },{
+//  age:14,
+//  salary:25000   
+// },{
+//     age:13,
+//     salary:25000
+// }]
+
+// console.table(arun)
+// let code =arun.map((v)=>{ return v.age+1})
+// console.log(code)
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(-1, -2);
+// console.log(citrus)
+// const arr=[0, 1, 2, 3, 4, 5, 6, 7, 8];
+//  arr.splice(2,2,"Lemon","hai");
+// console.log(arr)
+// const fruits = [1,2,3,4];
+
+// At position 2, add 2 elements: 
+// fruits.splice(2, 0, "Lemon");
+// console.log(fruits)
+// const arun= [0, 1, 2, 3, 4, 5, 6, 7, 8];
+// const mohan=arun.slice();
+// console.log(mohan)
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(2, 5);
+// console.log(citrus)
+
+// const arun="hello every one this is arunakumar reddy chitha "
+// const redaty=arun.replaceAll(" ","_")
+// console.log(redaty)
+
+
+
+// try{
+// var a= ["34","32","5","31","24","44","67"]; //a is an array
+//    // displays elements of a
+  
+// if(a.length==3){
+//     console.log("hai")
+// }
+// else if (a.length==4){
+//     console.log("sjjs")
+// }
+// else[
+//     console.log("kao")
+// ]
+//  //b is undefined but still trying to fetch its value. Thus catch block will be invoked
+// }catch(e){
+// console.log("There is error which shows "+e.message); //Handling error
+// }
+
+
+
+// let a=50;
+// switch(a){
+//     case a=3:
+//         console.log("hao")
+//         break;
+//         case a<50:
+//             console.log("hello")
+//             break;
+//             default:
+//                 console.log("ja")
+// }
+
+
+let arun="waste"
+switch(arun){
+    case "hai every one" :
+            console.log("hello")
+            break;
+            case "welcome to the world of coding":
+                    console.log("thank youu")
+                    break;
+                    default :
+                    console.log("wastee")
+}
+
+
+
+
+// let date=new Date.day() 
+// console.log('date',date)
+
+
+
+
+// let arun=''
+// for(let i=0;i<=5;i++){
+//     arun+=i
+//     console.log(arun)
+//     console.log(typeof arun)
+    
+// }
+
+
+
+// function arun(){
+// document.getElementById("demo").innerHTML="arun reddy"
+// }
+// arun()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
